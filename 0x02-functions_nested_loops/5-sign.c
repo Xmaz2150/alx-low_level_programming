e69d717fafec4d88797a69d8794851aa9b407f5b
Xmaz2150 <muzirat0@gmail.com>
2023-02-21 15:11:29 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,22 @@
 
 int print_sign(int n)
 {
-	if (n > 0){
-		_putchar(43);
+	if (n > 0)
+	{	_putchar(43);
 		return (1);
 	}
-	else if (n == 0){	
+	else if (n == 0)
+	{	
 		_putchar(48);
 		return (0);
 	}
-	else if (n < 0){	
+	else if (n < 0)
+	{	
 		_putchar(45);
 		return (-1);
 	}
-	else{
+	else
+	{
 		return (0);
 	}
 }

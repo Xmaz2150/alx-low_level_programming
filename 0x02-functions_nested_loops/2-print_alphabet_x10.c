05530a189d9821efd5ef7bae02c27a9342b66756
Xmaz2150 <muzirat0@gmail.com>
2023-02-21 14:08:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
+}

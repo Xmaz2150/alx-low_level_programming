400c54912abe69e782faa7cc0a26662267f11cdd
Xmaz2150 <muzirat0@gmail.com>
2023-02-21 06:01:43 -0800

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int line[7];
+	int i;
+
+	line[0] = 95;
+	line[1] = 112;
+	line[2] = 117;
+	line[3] = 116;
+	line[4] = 99;
+	line[5] = 104;
+	line[6] = 97;
+	line[7] = 114;
+
+	for (i = 0 ;i <= 7 ;i++)
+	{
+		putchar(line[i]);
+	}
+	putchar(10);
+	return (0);
+}

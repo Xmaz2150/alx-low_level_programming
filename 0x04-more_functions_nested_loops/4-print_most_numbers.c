dc93b0492811e3339ea8a8bf48ccf83071f689d8
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-03-01 00:54:01 +0200

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * print_most_numbers - prints mmost no's?
+ *
+ * Return: none
+ */
 void print_most_numbers(void)
 {
         int i;
 
-        for (i = 48;i <= 57; i++)
+        for (i = 48 ;i <= 57; i++)
         {	
 		if (i == 50 || i == 52)
 		{

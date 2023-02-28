40d3adc120fe7e1b296669c99396228895dbe0f6
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-03-01 01:30:44 +0200

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,13 @@
 #include "main.h"
-
+/**
+ * print_line - prints line?
+ * @n : Input
+ * Return: none
+ */
 void print_line(int n)
 {	
 	int i;
-
+	
 	for (i = 0; i < n; i++)
 	{
 		_putchar('_');

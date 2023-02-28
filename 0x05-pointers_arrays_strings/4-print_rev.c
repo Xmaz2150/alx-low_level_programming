5a43d5793694ff15fc0ede4de56c06aa20417515
Xmaz2150 <muzirat0@gmail.com>
2023-02-28 05:30:56 -0800

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -18,11 +18,7 @@ void print_rev(char *s)
 		}
 	}
 
-<<<<<<< HEAD
 	for (j = 0; j < len +1; j++)
-=======
-	for (j = 1; j < len; j++)
->>>>>>> b018c6bfd0ee5d5c006d2d1b4264a0fc8a6e4478
 	{
 		_putchar(s[len - j]);
 	}

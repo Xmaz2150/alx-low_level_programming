6e7bd8cfba0bbdda7445ebb8d97c849a10a91614
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-03-01 01:01:06 +0200

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,13 @@ void more_numbers(void)
 
 	i = 0;
 
-	while(i <= 10)
+	while (i <= 10)
 	{
 		int j;
 
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j+'0');
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 		i++;

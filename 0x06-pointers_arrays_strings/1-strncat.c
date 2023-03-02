599c14380f5d202c4cbd93646b5e104f45028d9b
Xmaz2150 <muzirat0@gmail.com>
2023-03-02 03:57:07 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * *_strncat - concatenates two strings:
  *
  * @dest: Input and :
- * @src: , based on 
+ * @src: based on
  * @n: Input
- * 
+ *
  * Returns: char
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k, len_dest, len_src;
+	int i, k, len_dest;
 
 	len_dest = 0;
-	len_src = 0;
 	for (i = 0; ; i++)
 	{
 		if (dest[i] != '\0')
@@ -25,26 +25,16 @@ char *_strncat(char *dest, char *src, int n)
 			break;
 		}
 	}
-	for (j = 0; ; j++)
-        {
-                if (src[j] != '\0')
-                {
-                        len_src++;
-                }
-                else
-                {
-                        break;
-                }
-        }
-
 	for (k = 0; k < n; k++)
 	{
-		*(dest + (len_dest + k)) = src[k];
-		if (k < len_src)
+		if (src[k] != '\0')
+		{
+			*(dest + (len_dest + k)) = src[k];
+		}
+		else
 		{
 			break;
 		}
 	}
-	
 	return (dest);
 }

0a789fae947f12b6509d63372bcc8400468a0078
Xmaz2150 <muzirat0@gmail.com>
2023-03-03 17:08:46 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,34 +9,34 @@
  */
 char *leet(char *str)
 {
-int i;
+	int i;
 
-i = 0;
-        while (str[i] != '\0')
-        {
-                char c = str[i];
-		
+	i = 0;
+	while (str[i] != '\0')
+	{
+	char c = str[i];
+
 		if (c == 'a' || c == 'A')
-                {
+		{
 			str[i] = '4';
-                }
+		}
 		else if (c == 'e' || c == 'E')
-                {
-                        str[i] = '3';
-                }
+		{
+			str[i] = '3';
+		}
 		else if (c == 'o' || c == 'O')
-                {
-                        str[i] = '0';
-                }
+		{
+			str[i] = '0';
+		}
 		else if (c == 't' || c == 'T')
-                {
-                        str[i] = '7';
-                }
+		{
+			str[i] = '7';
+		}
 		else if (c == 'l' || c == 'L')
-                {
-                        str[i] = '1';
-                }
-                i++;
-        }
-        return (str);
+		{
+			str[i] = '1';
+		}
+		i++;
+	}
+	return (str);
 }

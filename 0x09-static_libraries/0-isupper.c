3a232e0f6c013b28ad131f33bb81b6ddfc4ba36f
Xmaz2150 <muzirat0@gmail.com>
2023-03-10 11:47:46 -0800

    My static library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - is upper
+ *
+ * @c: Input
+ * Return: none
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _puts - prints str
+ *
+ * @str: Input
+ * Return: none
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			_putchar('\n');
+			break;
+		}
+	}
+}

aa01256298199a0786f329cd1a4a23bc566159e3
Xmaz2150 <muzirat0@gmail.com>
2023-03-13 15:42:28 -0700

    0-create array,malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,15 +12,16 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 	
-	if (size = 0)
+	if (size == 0)
 	{
-		return ('\0');
+		return (0);
 	}
 	else
 	{
+		
 		a = malloc(sizeof(c)*size);
-		i = 0;
-
+	
+		i = 0;	
 		for (i = 0; i < size; i++)
 		{
 			a[i] = c;

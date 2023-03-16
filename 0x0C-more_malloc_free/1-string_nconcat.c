2c2526a10d27cdefa3125231dd9acc00ed079ce4
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-03-17 00:07:31 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,56 +14,56 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int s1_len, s2_len, i, j;
-        char *new_str;
+	unsigned int s1_len, s2_len, i, j;
+	char *new_str;
 
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-        if (s2 == NULL)
-        {
-                s2 = "";
-        }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-        s1_len = strlen(s1);
-        s2_len = strlen(s2);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-        new_str = malloc(s1_len + s2_len + 1);
+	new_str = malloc(s1_len + s2_len + 1);
 
-        if (new_str == NULL)
-        {
-                return (NULL);
-        }
-        else
-        {
-                i = 0;
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		i = 0;
 
-                while (i < s1_len)
-                {
-                        new_str[i] = s1[i];
-                        i++;
-                }
+		while (i < s1_len)
+		{
+			new_str[i] = s1[i];
+			i++;
+		}
 
-                j = 0;
+		j = 0;
 
-                if (n < s2_len)
-                {
-                        while (j < n)
-                        {
-                                new_str[i + j] = s2[j];
-                                j++;
-                        }
-                }
-                else if (n <= s2_len)
-                {
-                        while (j < s2_len)
-                        {
-                                new_str[i + j] = s2[j];
-                                j++;
-                        }
-                }
-                new_str[i + j] = '\0';
+		if (n < s2_len)
+		{
+			while (j < n)
+			{
+				new_str[i + j] = s2[j];
+				j++;
+			}
+		}
+		else if (n <= s2_len)
+		{
+			while (j < s2_len)
+			{
+				new_str[i + j] = s2[j];
+				j++;
+			}
+		}
+		new_str[i + j] = '\0';
 		return (new_str);
 	}
 }

aea80ebc2282be102816a9455afecc6a45689c54
Xmaz2150 <muzirat0@gmail.com>
2023-03-16 14:58:00 -0700

    1-str n concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat -returns new string with combined str's:
+ *
+ * @s1: Input and
+ * @s2: Input, until
+ * @n: Input bytes
+ *
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+        unsigned int s1_len, s2_len, i, j;
+        char *new_str;
+
+        if (s1 == NULL)
+        {
+                s1 = "";
+        }
+        if (s2 == NULL)
+        {
+                s2 = "";
+        }
+
+        s1_len = strlen(s1);
+        s2_len = strlen(s2);
+
+        new_str = malloc(s1_len + s2_len + 1);
+
+        if (new_str == NULL)
+        {
+                return (NULL);
+        }
+        else
+        {
+                i = 0;
+
+                while (i < s1_len)
+                {
+                        new_str[i] = s1[i];
+                        i++;
+                }
+
+                j = 0;
+
+                if (n < s2_len)
+                {
+                        while (j < n)
+                        {
+                                new_str[i + j] = s2[j];
+                                j++;
+                        }
+                }
+                else if (n <= s2_len)
+                {
+                        while (j < s2_len)
+                        {
+                                new_str[i + j] = s2[j];
+                                j++;
+                        }
+                }
+                new_str[i + j] = '\0';
+		return (new_str);
+	}
+}

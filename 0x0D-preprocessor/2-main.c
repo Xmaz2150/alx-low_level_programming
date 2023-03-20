563234738132db7ec8f15afbd52e291da690c34a
Xmaz2150 <muzirat0@gmail.com>
2023-03-20 11:56:06 -0700

    2-main.c file name update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,18 +1,12 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * main - prints name of the program it is compiled from
  * Return: int
  */
 int main(void)
 {
-	char my_name[256];
-	int size;
 
-	size = sizeof(my_name);
-
-	_strncpy(my_name, __FILE__, size);
-	_puts(my_name);
-
+	printf("%s\n", __FILE__);
 	return (0);
 }

ac81e38c24d10fa403d35ee68a3aa9d39815453e
Xmaz2150 <muzirat0@gmail.com>
2023-03-21 10:53:22 -0700

    dog.h update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG
 #define DOG
 
+/**
+ * struct dog -defines a new struct with members
+ * @name
+ * @age
+ * @owner
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+}
+dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

69683b0beaf4e96a82f774b563f1e9c0b46bfaea
Xmaz2150 <muzirat0@gmail.com>
2023-03-23 06:01:17 -0700

    print name with call back funtion (NULL) case

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 /**
  * print_name - prints:
  *
@@ -10,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }

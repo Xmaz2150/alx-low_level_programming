0f46a95d1383f20c5b1514b03fdfe5ca4e98f545
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-04-20 08:43:52 -0700

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,5 +0,0 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-
-#define ABS(x) x < 0 ? (-1*x) : x
-#endif /*FUNCTION_LIKE_MACRO*/
--- 0x0D-preprocessor/main.h
+++
@@ -1,3 +0,0 @@
-int _putchar(char c);
-char *_strncpy(char *dest, char *src, int n);
-void _puts(char *str);

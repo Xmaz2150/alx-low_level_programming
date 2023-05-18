066dc687b13310c9d80248915f7a1759607608d2
Taf <muzirat0@gmail.com>
2023-05-17 22:43:42 -0700

    prints message to std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,14 @@
 /*
  * main - Etry point
  *
- * Return: 1
+ * Return: Alwasy 1 (Success)
  */
 
 int main(void)
 {
-	const char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *s;
+
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, s, strlen(s));
 
 	return (1);

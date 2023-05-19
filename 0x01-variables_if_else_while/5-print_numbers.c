73e06f912fb8bdfef9a50654c39f698b71dc6677
Taf <muzirat0@gmail.com>
2023-05-19 15:00:47 -0700

    prints 1 - 10
    base 10
    fixed betty coding err

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,11 +6,14 @@
  */
 int main(void)
 {
-        int i;
-        for (i = 0;i < 10;i++)
-        {
-                printf("%i", i);
-        }
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
 	printf("\n");
-        return (0);
+
+	return (0);
 }

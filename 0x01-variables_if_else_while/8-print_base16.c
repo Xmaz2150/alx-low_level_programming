61e128ff4b2223578b98f8ce7fb7b0c1d89a6ab6
Taf <muzirat0@gmail.com>
2023-05-19 15:15:48 -0700

    prints 1 - 16 in hex
    base 16
    fixed betty coding err

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,18 +7,21 @@
 
 int main(void)
 {
-  int i;
-  for(i = 0; i <= 15; i++)
-  {
-    if(i < 10)
-    {
-      putchar(i + '0');
-    }
-    else
-    {
-      putchar(i - 10 + 'a');
-    }
-  }
-  putchar('\n');
-  return 0;
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }

728f6af4d71fe37eeef8f073451af9b7cfb3c18d
Taf <muzirat0@gmail.com>
2023-05-19 14:48:09 -0700

    prints a - z
    fixed betty coding err

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,12 @@
 
 int main(void)
 {
-    char i;
-    
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
-    
-    return 0;
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }

50611f4b3d583f70b915c008d44b400ec2cd1d7b
Taf <muzirat0@gmail.com>
2023-05-19 14:52:10 -0700

    prints a - z & A - Z
    fixed betty codind err

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	char i;
 	char j;
-    
-   	for (i = 'a'; i <= 'z'; i++)
-    	{
-        	putchar(i);
-    	}
-    
-    	for (j = 'A'; j <= 'Z'; j++)
-    	{
-        	putchar(j);
-    	}
-    	putchar('\n');
-	
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
 	return (0);
 }

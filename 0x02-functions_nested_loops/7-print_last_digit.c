b1e6a05d34fd270836045e156f4c1cfac87aff3f
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-23 13:49:25 -0700

    returns and prints last digit of no

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,15 @@
 #include "main.h"
-
+/**
+ * print_last_digit - returns and prints last digit of
+ * @n: Input
+ *
+ * Return: las digit
+ */
 int print_last_digit(int n)
 {
-	return (n%10);
+	int last_d;
+	
+	last_d = n % 10;
+	_putchar('0' + last_d);
+	return (last_d);
 }

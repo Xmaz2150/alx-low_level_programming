80207d00ff93ad996f958ff4c52a3089f08065e9
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-23 14:18:37 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int i, j;
 	
-	for (int i = 0; i <= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (int j = 0; j <= 59; j++)
+		for (j = 0; j <= 59; j++)
 		{
 			if (i <= 9)
 				putchar('0');

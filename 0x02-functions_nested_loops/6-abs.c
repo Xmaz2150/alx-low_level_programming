8279509f73d586323f5138a9a0d06d2d6f68dd86
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-23 13:40:55 -0700

    prints abs value of int

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,15 @@
 #include "main.h"
-
+/**
+ * _abs - computes abs value of int
+ * @n: Input
+ *
+ * Return: -n or n
+ */
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return (-n);
+		return (- n);
 	}
 	else 
 	{

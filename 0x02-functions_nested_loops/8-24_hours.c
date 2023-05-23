35c5a0651fe8d40842d43b9d764c4291b89bfaeb
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-23 14:16:40 -0700

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,31 @@
 #include "main.h"
-
+/**
+ * print_last_digit - returns and prints last digit of
+ * @n: Input
+ *
+ * Return: las digit
+ */
 void jack_bauer(void)
 {
 	int i, j;
-
-	for (i = 0; i <= 59; i++)
+	
+	for (int i = 0; i <= 23; i++)
 	{
-		putchar((char)i);
+		for (int j = 0; j <= 59; j++)
+		{
+			if (i <= 9)
+				putchar('0');
+			printf("%d", i);
+			
+			putchar(':');
+			
+			if (j <= 9)
+				putchar('0');
+			printf("%d", j);
+			
+			if (j != 59)
+				putchar('\n');
+		}
+		putchar('\n');
+	}
+}

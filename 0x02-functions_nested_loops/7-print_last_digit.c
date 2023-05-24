3218216c916c72f9b489a65d8932f22cef7040f8
Taf <muzirat0@gmail.com>
2023-05-24 04:54:46 -0700

    returns and prints last digit of no
    handled both cases where int is < 0 | > 0

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,14 @@ int print_last_digit(int n)
 {
 	int last_d;
 
-	if (n < 0)
-		n *= -1;
-
 	last_d = n % 10;
+
+	if (n < 0)
+	{
+		_putchar('0' + (last_d * -1));
+		return (last_d * -1);
+	}
+
 	_putchar('0' + last_d);
 	return (last_d);
 }

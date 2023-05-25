33fb741d506651f8e377f7c7ff139e00d797b7a7
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-25 13:14:40 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - prints fizz buzz
+ *
+ * Return: (int) 0
+ */
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{	
+	{
 		if (i%3 == 0)
 		{
 			printf("Fizz ");
@@ -23,6 +27,6 @@ int main(void)
 		printf("%d ", i);
 	}
 	printf("\n");
-	
+
 	return (0);
 }

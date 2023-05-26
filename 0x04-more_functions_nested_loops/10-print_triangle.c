9daaaa6c0f183a8aa06fab218749e5f378bb7fba
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-05-26 10:33:52 -0700

    prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,17 +2,25 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
+	if (size > 0)
+	{
+		int i, j, k;
 
-	for (i = 0; i < size ; i++)
-	{	for (k = 1; k <size - i; k++)
-		{
-			_putchar(' ');
+		for (i = 0; i < size ; i++)
+		{	
+			for (k = 1; k <size - i; k++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (j = 0; j < i + 1; j++)
-		{
-			_putchar('#');
-		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }

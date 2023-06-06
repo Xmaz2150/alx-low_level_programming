f621855e20216b453a382ea22df980c2925c9c92
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-06-06 14:00:53 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,42 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len_a, len, i, si, ai;
+	unsigned int len_s;
+	unsigned int len_a;
+	unsigned int len;
+	unsigned int i;
+	unsigned int j;
+	unsigned int si;
+	unsigned int ai;
 
 	len = 0;
 	for (i = 0; ; i++)
 	{
-		if (accept[i] != '\0')
+		if (s[i] != '\0')
 		{
-			len_a++;
+			len_s++;
 		}
 		else
 		{
 			break;
 		}
 	}
+	for (j = 0; ; j++)
+        {
+                if (accept[j] != '\0')
+                {
+                        len_a++;
+                }
+                else
+                {
+                        break;
+                }
 
-	for (si = 0; s[si] != '\0'; si++)
+	}
+
+	for (si = 0; i < len_s; si++)
 	{
 		unsigned int match = 0;
-
 		for (ai = 0; ai < len_a; ai++)
 		{
 			if (accept[ai] == s[si])

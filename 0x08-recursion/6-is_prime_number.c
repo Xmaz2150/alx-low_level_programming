3ecc01931c264078f707548860100e22c64a19c1
Taf <muzirat0@gmail.com>
2023-06-08 04:44:06 -0700

    checks if num is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,14 +5,17 @@
  *
  * Return: int
  */
+int divider(int num, int div)
+{
+	if (div == 1)
+		return (1);
+	if (num % div == 0)
+		return (0);
+	return (divider(num, div - 1));
+}
 int is_prime_number(int n)
 {
-	if ((n % 2) == 0)
-	{
+	if (n < 2)
 		return (0);
-	}
-	else
-	{
-		return (1);
-	}
+	return (divider(n, n - 1));
 }

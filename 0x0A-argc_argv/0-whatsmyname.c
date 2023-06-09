0010eb2d6488715d6a34d1a5ff880377ba12106e
Taf <muzirat0@gmail.com>
2023-06-09 05:57:39 -0700

    greets user

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * main - prints first arg
+ * @argc: no of arguments
+ * @argv: arguments
+ *
+ * Return: Always (0) success
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != '\0')
 	{
 		int i;
-	
+
 		i = 0;
 
 		while (argv[0][i] != '\0')

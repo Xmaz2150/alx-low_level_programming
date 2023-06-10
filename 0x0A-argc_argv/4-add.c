3e1d737150ef8801651aeb4d73e0aa89936f7ab0
Taf <muzirat0@gmail.com>
2023-06-10 09:28:00 -0700

    adds all positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds all positive numbers
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: (0) Always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (atoi(argv[i]) > 0) 
+			sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

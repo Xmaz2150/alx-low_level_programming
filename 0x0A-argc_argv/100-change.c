4676709a74b53e4eb3440f80e68a89eba7f2ac57
Xmaxz2150 <113725438+Xmaz2150@users.noreply.github.com>
2023-06-10 19:28:43 +0200

    prints fewest coins ap

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints min no of coins
+* @argc: 2 arguments
+* @argv: prog name, cents
+*
+* Return: (0) Always, success
+*/
+int main(int argc, char *argv[])
+{
+int cents, coins;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (0);
+}
+if (!atoi(argv[1]))
+{
+printf("Error2\n");
+return (0);
+}
+
+cents = atoi(argv[1]);
+coins = change(cents);
+
+printf("%d\n", coins);
+return(0);
+}
+
+/**
+* change - simplifies main
+* @cents: Input
+*
+* Return: int
+*/
+int change(int cents)
+{
+while (cents >= 25)
+{
+cents -= 25;
+coins++;
+}
+
+while (cents >= 10)
+{
+cents -= 10;
+coins++;
+}
+
+while (cents >= 5)
+{
+cents -= 5;
+coins++;
+}
+while (cents >= 2)
+{
+cents -= 2;
+coins++;
+}
+if (cents == 1)
+coins++;
+return (coins);
+}

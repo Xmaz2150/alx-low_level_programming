cca9cf64e1de8b9e44e00d5f9bc07f4522fc3783
Taf <muzirat0@gmail.com>
2023-06-16 06:18:59 -0700

    concat s1 with n bytes of s2
    This reverts commit 44efda85dd55f7ef111ae55a1ef2a4514ac2d478.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-int my_strcpy(unsigned int i, unsigned int s_len, char *dest, char *s);
+
+/**
+unsigned int my_strcpy(unsigned int i, unsigned int s_len, char *dest, char *s)
+{
+	unsigned int counter;
+
+	counter = 0;
+	while (counter < s_len)
+	{
+		dest[counter + i] = s[counter];
+		counter++;
+	}
+	return (i + counter);
+}
+*/
 /**
  * string_nconcat -returns new string with combined str's:
  *
@@ -21,46 +34,50 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 	{
 		s1 = malloc(1);
+		if (s1 == NULL)
+			return(NULL);
 		*s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
 		s2 = malloc(1);
+		if (s2 == NULL)
+			return (NULL);
 		*s2 = '\0';
 	}
+
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
+
 	new_str = malloc(s1_len + s2_len + 1);
+
 	if (new_str == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		i = my_strcpy(0, s1_len, new_str, s1);
-		if (n < s2_len)
-		{
-			j = my_strcpy(i, n, new_str, s2);
-		}
-		else if (n >= s2_len)
-		{
-			j = my_strcpy(i, s2_len, new_str, s2);
-		}
-		new_str[i + j] = '\0';
-		printf("s1 len: %d\ns2 len: %d\ni: %d\nj: %d\n", s1_len, s2_len, i, j);
-		return (new_str);
-	}
-}
+	i = 0;
 
-int my_strcpy(unsigned int i, unsigned int s_len, char *dest, char *s)
-{
-	unsigned int counter;
-
-	counter = 0;
-	while (counter < s_len)
+	while (i < s1_len)
 	{
-		dest[counter + i] = s[counter];
-		counter++;
+		new_str[i] = s1[i];
+		i++;
 	}
-	return (i + counter);
+	j = 0;
+
+	if (n < s2_len)
+	{
+		while (j < n)
+		{
+			new_str[i + j] = s2[j];
+			j++;
+		}
+	}
+	else if (n <= s2_len)
+	{
+		while (j < s2_len)
+		{
+			new_str[i + j] = s2[j];
+			j++;
+		}
+	}
+	new_str[i + j] = '\0';
+	return (new_str);
 }

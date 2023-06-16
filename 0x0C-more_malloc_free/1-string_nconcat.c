78bc876e62b020726c55369bda0d5a436acc541b
Taf <muzirat0@gmail.com>
2023-06-16 05:24:10 -0700

    concats s1 with n bytes of s2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
+int my_strcpy(unsigned int i, unsigned int s_len, char *dest, char *s);
 /**
  * string_nconcat -returns new string with combined str's:
  *
@@ -27,45 +28,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = malloc(1);
 		*s2 = '\0';
 	}
-
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-
 	new_str = malloc(s1_len + s2_len + 1);
-
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
 	else
 	{
-		i = 0;
-
-		while (i < s1_len)
-		{
-			new_str[i] = s1[i];
-			i++;
-		}
-
-		j = 0;
-
+		i = my_strcpy(0, s1_len, new_str, s1);
 		if (n < s2_len)
 		{
-			while (j < n)
-			{
-				new_str[i + j] = s2[j];
-				j++;
-			}
+			j = my_strcpy(i, n, new_str, s2);
 		}
 		else if (n >= s2_len)
 		{
-			while (j < s2_len)
-			{
-				new_str[i + j] = s2[j];
-				j++;
-			}
+			j = my_strcpy(i, s2_len, new_str, s2);
 		}
 		new_str[i + j] = '\0';
+		printf("s1 len: %d\ns2 len: %d\ni: %d\nj: %d\n", s1_len, s2_len, i, j);
 		return (new_str);
 	}
+}
+
+int my_strcpy(unsigned int i, unsigned int s_len, char *dest, char *s)
+{
+	unsigned int counter;
+
+	counter = 0;
+	while (counter < s_len)
+	{
+		dest[counter + i] = s[counter];
+		counter++;
+	}
+	return (i + counter);
 }

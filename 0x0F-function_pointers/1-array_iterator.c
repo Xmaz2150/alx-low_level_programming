d79fdb5b9f74a1b9d0e84ece7660cc41d732d726
Taf <muzirat0@gmail.com>
2023-06-21 08:23:16 -0700

    executes different operations(action) on arr

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 #include <stdio.h>
+/**
+ * array_iterator - performs different operations on:
+ * @array: Input, of size:
+ * @size: Input.
+ * @action: Input, operations.
+ *
+ * Return: (void)
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;

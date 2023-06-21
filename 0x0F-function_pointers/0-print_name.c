46859651da2dbf351d0d491de347c20754cdb551
Taf <muzirat0@gmail.com>
2023-06-21 07:59:50 -0700

    prints name checks if args are valid

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints name:
  * @name: Input, with function at:
@@ -9,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if ((name != NULL) || (f != NULL))
+		f(name);
+	return (NULL);
 }

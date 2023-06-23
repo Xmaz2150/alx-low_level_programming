d0dd4750518d5f41ddd56e5d09fd3e4b5c2fafba
Taf <muzirat0@gmail.com>
2023-06-23 01:38:25 -0700

    prints number separated by separator
    print numbers only if separator is NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (separator == NULL)
-		return;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i < n - 1)
+		if ((i < n - 1) && (separator != NULL))
 		{
 			printf("%d%s", va_arg(ap, int), separator);
 		}
 		else
 		{
-			printf("%d", va_arg(ap, int));
+				printf("%d", va_arg(ap, int));
 		}
 	}
 	printf("\n");

30abc84c285c91b8fbde9846d4ae9575be9e8efc
Taf <taf@Ubuntu-22.04.1.myguest.virtualbox.org>
2023-07-04 14:50:34 -0700

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,13 +21,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (*head == NULL)
+	if  (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
-	while (tmp != NULL)
+	while (tmp->next != NULL)
 		tmp = tmp->next;
 
 	tmp->next = new_node;

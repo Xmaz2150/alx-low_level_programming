b3d619796929ff923abfe9e78396e85a90eed857
Taf <taf@Ubuntu-22.04.1.myguest.virtualbox.org>
2023-07-04 14:59:16 -0700

    free lls

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees lls at:
+ * @head: Input, start of lls
+ *
+ * Return: (void) none
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(tmp);
+
+		head = head->next;
+	}
+}

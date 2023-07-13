01e79eb36575336fb120e52c9784f06867282e07
taf <muzirat0@gmail.com>
2023-07-13 15:07:00 +0000

    flip n bits to go from num1 to num2

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+int count_set_bits(int n)
+{
+	int count;
+	
+	count = 0;
+	while (n > 0) 
+	{
+		count++;
+		n &= (n - 1);
+	}
+	return count;
+}
+
+/**
+ * flip_bits - ruturns no of bits to be flipped to go from:
+ * @n: Input, num1 to:
+ * @m: Input, num2
+ *
+ * Return: (u int) no of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countSetBits(n ^ m));
+}

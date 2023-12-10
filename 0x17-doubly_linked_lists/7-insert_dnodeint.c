44ecdb6bd12ab74b86378ff1bfb79b3cb1845fdf
taf <muzirat0@gmail.com>
2023-12-10 14:17:42 -0800

    insert node at index
    backward pointing

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,31 +9,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	else if (idx >= dlistint_len(*h))
 		return (add_dnodeint_end(h, n));
-	else
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	current_node = *h;
+	i = 0;
+	while (i != (idx - 1))
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
-		new_node->next = NULL;
-
-		current_node = *h;
-		i = 0;
-		while (i != (idx - 1))
-		{
-			current_node = current_node->next;
-			i++;
-		}
-
-		/* forward */
-		new_node->next = current_node->next;
-		current_node->next = new_node;
-
-		/* backwards */
-
-		return (new_node);
+		current_node = current_node->next;
+		i++;
 	}
+
+	/* forward */
+	new_node->next = current_node->next;
+	/* backwards */
+	new_node->prev = current_node;
+
+	current_node->next = new_node;
+
+	return (new_node);
 }
 
 /**

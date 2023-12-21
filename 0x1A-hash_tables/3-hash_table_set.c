6563a6d9cd3a1868482282214a32e91ddd55814e
root <root@DESKTOP-K7H8GF6>
2023-12-21 22:33:37 +0200

    adds element to a hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - inserts new key and corresponding val
+ * @ht: Input, hash table
+ * @key: Input, key
+ * @value: Input, val
+ *
+ * Return: status (1) Success
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	int idx;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	idx = key_index((unsigned const char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	ht->array[idx] = new_node;
+	return (1);
+}

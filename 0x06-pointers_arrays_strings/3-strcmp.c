6be6f9d85692305ae14043067aa928dbf1d97168
tAf <muzirat0@gmail.com>
2024-03-05 11:18:46 +0200

    3-strcmp v2
    correct return val

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,21 +17,17 @@ int _strcmp(char *s1, char *s2)
 		{
 			break;
 		}
-		if (s1[i] == s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (0);
-		}
-		else
-		{
-			if (s1 > s2)
+			if (s1[i] > s2[i])
 			{
-				return (15);
+				return (13);
 			}
 			else
 			{
-				return (-15);
+				return (-13);
 			}
 		}
 	}
-	return (-1);
+	return (0);
 }

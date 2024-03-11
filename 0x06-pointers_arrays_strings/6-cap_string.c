4decac561b0a0a379c7c4884efb4490477712f7e
tAf <muzirat0@gmail.com>
2024-03-11 08:24:03 +0200

    cap str

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,19 +9,14 @@
  */
 char *cap_string(char *str)
 {
-	char *separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int i;
+int i;
 
-	i = 0;
+i = 0;
 	while (str[i] != '\0')
 	{
 		char c = str[i];
 
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i + 1] -= 32;
-		}
-		if (is_sep(c, separators))
+		if (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' || c == '.' || c == '!' || c == '?' || c == '"' || c == '(' || c == ')' || c == '{' || c == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
@@ -31,16 +26,4 @@ char *cap_string(char *str)
 		i++;
 	}
 	return (str);
-}
-
-int is_sep(char c, char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == c)
-			return (1);
-	}
-	return (0);
 }

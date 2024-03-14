3d5b2ecdbe44c7d5aa5748224cbab47e05c73c1b
tAf <muzirat0@gmail.com>
2024-03-14 11:32:52 +0200

    leets
    encodes a string into 1337.
    
    Letters a and A replaced by 4
    Letters e and E replaced by 3
    Letters o and O replaced by 0
    Letters t and T replaced by 7
    Letters l and L replaced by 1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+
+char leet_it(leet_t *arr, char c);
+
 /**
- * *leet - changes certain letters to no's
+ * leet - changes certain letters to no's
  *
  * @str: Input
  *
@@ -11,32 +14,43 @@ char *leet(char *str)
 {
 	int i;
 
+	leet_t arr[] = {
+		{"Aa", '4'},
+		{"Ee", '3'},
+		{"Oo", '0'},
+		{"Tt", '7'},
+		{"Ll", '1'},
+		{"", '\0'}
+	};
+
 	i = 0;
 	while (str[i] != '\0')
 	{
-	char c = str[i];
-
-		if (c == 'a' || c == 'A')
-		{
-			str[i] = '4';
-		}
-		else if (c == 'e' || c == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (c == 'o' || c == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (c == 't' || c == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (c == 'l' || c == 'L')
-		{
-			str[i] = '1';
-		}
+		str[i] = leet_it(arr, str[i]);
 		i++;
 	}
 	return (str);
+}
+
+/**
+ * leet_it - leet helper
+ * @arr: Input, leet struct arr
+ * @c: Input, char to leet
+ *
+ * Return: leet_c or default char
+ */
+char leet_it(leet_t *arr, char c)
+{
+	char leet_c1, leet_c2;
+	int i = 0;
+
+	while (arr[i].leet != '\0')
+	{
+		leet_c1 = arr[i].leet_chars[0];
+		leet_c2 = arr[i].leet_chars[1];
+		if (c == leet_c1 || c == leet_c2)
+			return (arr[i].leet);
+		i++;
+	}
+	return (c);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef struct my_struct {
+	char *leet_chars;
+	char leet;
+} leet_t;
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -7,3 +15,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *str);
 char *leet(char *str);
+
+#endif /*MAIN_H_ */

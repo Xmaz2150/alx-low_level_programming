3a682a7f4774ccd055ecf36647a758f660bacc6e
taf <muzirat0@gmail.com>
2024-06-18 03:50:13 -0700

    flag for checking if digits have started computing

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,21 +9,22 @@
 
 int _atoi(char *s)
 {
-	int i, mult, num;
+	int i, mult, num, started;
 
 	num = 0;
 	mult = 1;
+	started = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 			mult *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			started = 1;
 			num = num * 10 + (s[i] - '0');
-			if (!(s[i] >= '0' && s[i] <= '9'))
-				break;
 		}
+		else if (started)
+			break;
 	}
 	return (num * mult);
 }
